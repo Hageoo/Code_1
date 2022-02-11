d093fdb9a882a5ccee3d3ba46c5d85d05e7c5153
Hageo <2109012@upy.edu.mx>
2022-02-11 13:31:52 -0600

    add even program

--- Unit_1/inputs.c
+++ Unit_1/inputs.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
-int num1; //declarando variable
+int num1; //declarando variable//
 int num2;
 
 int main (){
 
     //imprimir mensaje//
-    printf("Enter number1,\n\tnum1: ");
-    //inputs//
+    printf("Enter number1, \n\tnum1: ");
     scanf("%d", &num1);
-    printf("Enter number2,\n\tnum2: ");
+    printf("Enter number2, \n\tnum2: ");
     scanf("%d", &num2);
-    int sum = num1 + num2;
-    printf("suma = %d\n", sum);
     return 0;
 }
