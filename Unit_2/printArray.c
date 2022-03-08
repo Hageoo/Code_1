1f666ac16ac7bce2672591d31bcca648e13d98d5
Hageo <2109012@upy.edu.mx>
2022-03-08 17:23:56 -0600

    array modified

--- Unit_2/printArray.c
+++ Unit_2/printArray.c
@@ -4,13 +4,17 @@
 int main(){
     int myArray[3]={5, 2, 3};
     int i = 0;
-
+    printf("[");
     void printArray(int myArray[3], int arrayLenght){
         while (i < 3){
-            printf ("%d\t", myArray[i]);
+            printf ("%d", myArray[i]);
+            if (i < 2){
+                printf(",");
+            }
             i++;
         }
     }
     printArray (myArray, 3);
+    printf("]\n");
     return 0;
 }
