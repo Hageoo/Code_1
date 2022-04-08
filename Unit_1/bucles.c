4a66a89826741bd8e46c4fe721a6485dfe640be6
Hageo <2109012@upy.edu.mx>
2022-04-08 12:34:42 -0500

    Activities

---
+++ Unit_1/bucles.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int count = 1;
+
+int main(){
+    printf("--while loop--\n");
+    while ( count <= 100) {
+        printf("count: %d\n", count);
+        count = count + 1;
+    }
+    printf("--for loop--\n");
+    for (int i = 1; i <= 100; i = i + 1 ) {
+        printf("i: %d\n", i);
+    }
+
+    for (int i = 1;  ; i = i +1) {
+        printf ("i: %d\n", count);
+        count = count +1;
+    }
+
+    while(true){
+        printf("hello\n");
+    }
+
+    return 0;  
+
+}
